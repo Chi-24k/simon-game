6e8eaed9d51da782a2024251379b3c008e382d0a
Chidimma <chidimmaokoyeuzu@gmail.com>
2025-04-27 21:05:57 +0100

    step 3 done

--- games.js
+++ games.js
@@ -5,4 +5,14 @@ function nextSequence() {
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  const selectButton = $('#' + randomChosenColour);
+  selectButton.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); //thank you stackoverflow hehe
+
+  const buttonAudio = new Audio('sounds/' + randomChosenColour + '.mp3');
+  buttonAudio.play();
 }
+
+$(document).keydown(function () {
+  nextSequence();
+});
